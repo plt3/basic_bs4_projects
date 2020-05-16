9a093c0c22d151405b71d9b2c4037506d700eafc
Paul Taylor <pltorigami@gmail.com>
2020-05-16 15:06:37 -0400

    youtube search results, user chooses video and it opens

--- searchyt.py
+++ searchyt.py
@@ -2,7 +2,7 @@ import requests
 import bs4
 import webbrowser
 
-usersearch = input('What would you like to search for on YouTube? ').split()
+usersearch = input('\nWhat would you like to search for on YouTube? ').split()
 urlstr = 'https://www.youtube.com/results?search_query=' + '+'.join(usersearch)
 
 gottenpage = requests.get(urlstr)
@@ -10,24 +10,17 @@ gottenpage.raise_for_status()
 
 ytsoup = bs4.BeautifulSoup(gottenpage.text, 'html.parser')
 
-rawlist = ytsoup.select('a', class_="yt-simple-endpoint style-scope ytd-video-renderer")
+rawlist = ytsoup.select('a[href^="/watch"][title]')
 
-vidlist = []
-urllist = []
+print('\n%d results found. How many would you like to display?' % len(rawlist), end=' ')
+useramt = int(input())
+print()
 
-for elem in rawlist:
-    elemurl = str(elem.get('href'))
-    if elemurl.startswith('/watch') and elemurl not in urllist:
-        vidlist.append(elem)
+for i in range(useramt):
+    print()
+    print(str(i + 1) + '. ' + rawlist[i].get('title'))
 
-    urllist.append(elemurl)
+usernum = int(input('\n\nEnter number of video you would like to watch: '))
 
-for video in vidlist[:4]:
-    print(video)
-    print()
-    print(video.get('href'))
-    print()
-    print(video.attrs)
-    print()
-    print('-----------------------')
-    print()
+chosenurl = 'https://www.youtube.com' + rawlist[usernum - 1].get('href')
+webbrowser.open(chosenurl)
