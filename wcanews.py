d6448dac07280060c3a02537820e6284c32c3d76
Paul Taylor <pltorigami@gmail.com>
2020-05-18 15:39:59 -0400

    finished wcanews.py writes news digest to file for user-specified keyword

--- wcanews.py
+++ wcanews.py
@@ -11,14 +11,14 @@ def preparesoup(url):
 
 
 def onlytext(soup):
-    paragraphs = soup.select('div[class="panel-body"] > p')
+    paragraphs = soup.select('div[class="panel-body"] > p, div[class="panel-body"] > ul')
     for tag in paragraphs:
-        print(tag.get_text())
+        print(tag.get_text(), file=newsfile)
 
 
 while True:
     try:
-        userinterest = input('\nEnter keywords of news to get from the wca: ')
+        userinterest = input('\nEnter keywords of news to get from the WCA: ')
         print()
 
         cubesoup = preparesoup('https://www.worldcubeassociation.org')
@@ -27,14 +27,20 @@ while True:
 
         interestedlist = []
 
+        newsfile = open('WCA news digest.txt', 'w')
+        print('WCA news digest for keyword ' + userinterest, end=':\n\n\n', file=newsfile)
+
         for story in allnews:
             if userinterest in story.get_text().lower():
+                if interestedlist != []:
+                    print('\n\n----------------------\n\n', file=newsfile)
+
                 interestedlist.append(1)
+
                 title = story.select('h3[class="panel-title"] > a')[0]
-                print(title.get_text())
+                print(title.get_text(), file=newsfile)
                 fullpagelink = 'https://www.worldcubeassociation.org' + title.get('href')
-                print(fullpagelink)
-                print()
+                print(fullpagelink + '\n', file=newsfile)
 
                 if 'Read more' in story.get_text():
                     longpage = preparesoup(fullpagelink)
@@ -42,12 +48,12 @@ while True:
                 else:
                     onlytext(story)
 
-                if allnews.index(story) != len(allnews) - 1:
-                    print('\n----------------------\n')
-
         if interestedlist == []:
             raise ValueError('No results found. Please try again.')
 
+        newsfile.close()
+        print(str(len(interestedlist)) + ' posts found that matched your interests. Look for a file')
+        print('named "WCA news digest.txt" in your current directory. ')
         break
 
     except ValueError as e:
