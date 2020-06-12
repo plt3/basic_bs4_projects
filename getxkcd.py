65f63b95c8aafc8230f106ecfd111e3f126e36af
Paul Taylor <pltorigami@gmail.com>
2020-06-11 23:04:07 -0400

    multithreaded getxkcd.py to download comics much faster

--- getxkcd.py
+++ getxkcd.py
@@ -2,13 +2,17 @@ import requests
 import bs4
 import re
 import os
+import time
+import threading
+
+# NOTE: like actually don't run this with more than 500 requests. That's not very nice to XKCD
 
 
 def preparesoup(site):
     xkcdpage = requests.get(site)
     xkcdpage.raise_for_status()
 
-    xkcdsoup = bs4.BeautifulSoup(xkcdpage.text, 'html.parser')
+    xkcdsoup = bs4.BeautifulSoup(xkcdpage.text, 'lxml')
 
     return xkcdsoup
 
@@ -16,7 +20,7 @@ def preparesoup(site):
 def download_img(url):
     fullpage = preparesoup(url)
 
-    images = fullpage.select('div[id="comic"] > img')
+    images = fullpage.select('div[id="comic"] img')
 
     comicimage = images[0].get('src')
 
@@ -32,42 +36,51 @@ def download_img(url):
             f.write(chunk)
 
 
-def firstlink():
+def firstNum():
     fullpage = preparesoup('https://xkcd.com')
     tag = fullpage.select('div[id="middleContainer"]')
     midstr = tag[0].get_text()
 
-    linkpattern = re.compile(r'https://xkcd.com/\d+/')
+    linkpattern = re.compile(r'(https://xkcd.com/)(\d+)')
     found = linkpattern.search(midstr)
 
-    return found.group()
+    return int(found.groups()[1])
 
 
-def prevlink(url):
-    fullpage = preparesoup(url)
-
-    prev = fullpage.select('a[rel="prev"]')
-
-    return 'https://xkcd.com' + prev[0].get('href')
+def fullDownload(start, stop):
+    for i in range(start, stop, -1):
+        link = f'https://xkcd.com/{i}/'
+        print('Downloading xkcd number ' + str(i), end=':\n')
+        download_img(link)
 
 
 def main():
-    useramt = int(input('How many xkcd comics would you like to download? '))
+    userAmt = int(input('How many xkcd comics would you like to download? '))
     print()
+    start = time.time()
 
     os.makedirs('xkcd comics')
 
-    link = firstlink()
-    numpattern = re.compile(r'\d+')
+    begin = firstNum()
 
-    for i in range(1, useramt + 1):
-        number = numpattern.search(link)
-        print('Downloading xkcd number ' + number.group(), end=':\n')
-        download_img(link)
-        prevone = prevlink(link)
-        link = prevone
+    oldNum = begin
 
+    threadList = []
+
+    for i in range(begin - 10, begin - userAmt, - 10):
+        threadObj = threading.Thread(target=fullDownload, args=[oldNum, i])
+        threadObj.start()
+        threadList.append(threadObj)
+        oldNum = i
+
+    fullDownload(oldNum, begin - userAmt)
+
+    for item in threadList:
+        item.join()
+
+    end = time.time()
     print('\nDone. Look for an "xkcd comics" folder in your current directory.')
+    print(f'done in {end - start} seconds')
 
 
 main()
